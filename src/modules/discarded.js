19fbc05025ec74266b302a01c3d84d762d026b77
utubo <utbutb@gmail.com>
2024-09-06 14:59:49 +0900

    🐞fix: discard without history, suppor dark theme

--- src/modules/discarded.js
+++ src/modules/discarded.js
@@ -2,7 +2,7 @@ const url = location.search.substring(1);
 document.title = url;
 document.addEventListener('visibilitychange', () => {
 	if (document.visibilityState === 'visible') {
-		location.href = url;
+		location.replace(url);
 	}
 });
 
