c33abb3daa0db4a88eba887e89d6db0250e95578
utubo <utbutb@gmail.com>
2024-10-15 17:50:46 +0900

    🔧chore: for manifest-v3

--- make.sh
+++ make.sh
@@ -8,13 +8,15 @@ zip -r ../simple-gesture.zip * -x "manifest_v3.json"
 cd ..
 mv -f simple-gesture.zip simple-gesture.xpi
 
+# for Kiwi browser
 # manifest_v3
 cd $SCRIPT_DIR/src
 mv manifest.json manifest_v2.json
 mv manifest_v3.json manifest.json
 zip -r ../simple-gesture-v3.zip * -x "manifest_v2.json"
 cd ..
-mv -f simple-gesture-v3.zip simple-gesture-v3.xpi
+# Kiwi supports .zip instead of .xpi
+# mv -f simple-gesture-v3.zip simple-gesture-v3.xpi
 cd src
 mv manifest.json manifest_v3.json
 mv manifest_v2.json manifest.json
