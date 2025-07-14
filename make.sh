0c1535be4b293bbe8e77b06b5f663f35c97fd47f
utubo <utbutb@gmail.com>
2025-07-14 10:33:46 +0900

    🔧chore: icon for beta

--- make.sh
+++ make.sh
@@ -6,13 +6,19 @@ cd $SCRIPT_DIR/src
 
 for_beta () {
 	v=$(grep -e '"version": ".*beta"' manifest.json)
+	echo $v
 	if [ -n "$v" ]; then
-		sed -e 's/beta//' -e 's/@/_beta@' manifest.json > manifest.json
+		sed -e 's/beta//' -e 's/@/_beta@/' manifest.json > manifest.json.beta
+		mv -f manifest.json.beta manifest.json
+		sed -e 's/#afc639/#9059ff/' icon64.svg > icon64.svg.beta
+		mv -f icon64.svg.beta icon64.svg
+		cat icon64.svg
 	fi
 }
 
-# Backup manifest.json
+# Backup manifest.json and icon
 mv manifest.json manifest_firefox.json
+cp -p icon64.svg ../icon64_backup.svg
 
 # Firefox
 cp -p manifest_firefox.json manifest.json
@@ -26,8 +32,9 @@ for_beta
 zip -r ../simple-gesture.zip * -x "manifest_*.json"
 mv -f ../simple-gesture.zip ../simple-gesture-chrome.zip
 
-# Restore manifest.json
+# Restore manifest.json and icon
 mv -f manifest_firefox.json manifest.json
+mv -f ../icon64_backup.svg icon64.svg
 
 cd $CURRENT_DIR
 
