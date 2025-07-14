d3785f323ea0ab45cd767b3edd45bd129edd3a38
utubo <utbutb@gmail.com>
2025-07-14 18:41:07 +0900

    🔧chore: refactor

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
+XPI_NAME=simple-gesture
 CURRENT_DIR=$(pwd)
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR/src
@@ -12,7 +13,6 @@ for_beta () {
 		mv -f manifest.json.beta manifest.json
 		sed -e 's/#afc639/#9059ff/' icon64.svg > icon64.svg.beta
 		mv -f icon64.svg.beta icon64.svg
-		cat icon64.svg
 	fi
 }
 
@@ -23,14 +23,14 @@ cp -p icon64.svg ../icon64_backup.svg
 # Firefox
 cp -p manifest_firefox.json manifest.json
 for_beta
-zip -r ../simple-gesture.zip * -x "manifest_*.json"
-mv -f ../simple-gesture.zip ../simple-gesture.xpi
+zip -r ../$XPI_NAME.zip * -x "manifest_*.json"
+mv -f ../$XPI_NAME.zip ../$XPI_NAME.xpi
 
 # Chrome
 cp -p manifest_chrome.json manifest.json
 for_beta
-zip -r ../simple-gesture.zip * -x "manifest_*.json"
-mv -f ../simple-gesture.zip ../simple-gesture-chrome.zip
+zip -r ../$XPI_NAME.zip * -x "manifest_*.json"
+mv -f ../$XPI_NAME.zip ../$XPI_NAME-chrome.zip
 
 # Restore manifest.json and icon
 mv -f manifest_firefox.json manifest.json
