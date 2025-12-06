a1c111217395bcce79ab3a8adeced30492f55a97
utubo <utbutb@gmail.com>
2025-12-06 11:36:49 +0900

    📝docs: fix a comment.

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR/src
 
 # Fix manifest.json for beta
-# NOTE: strict_min_version of Water fox is 140.0
+# NOTE: keep strict_min_version for ESR.
 for_beta () {
 	if [ -n "$MAKE_XPI_BETA" ]; then
 		sed \
