094efc742cb4761092546b6931ac9343e5ced635
utubo <utbutb@gmail.com>
2026-05-15 13:19:40 +0900

    🔧chore: build beta version

--- build.sh
+++ build.sh
@@ -5,27 +5,22 @@ CURRENT_DIR=$(pwd)
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR/src
 
-# Fix manifest.json for beta
-# NOTE: keep strict_min_version for ESR.
-for_beta () {
-	if [ -n "$MAKE_XPI_BETA" ]; then
-		sed \
-			-e 's/"version": "\(.*\)"/"version": "\1"\, "version_name": "\1 beta"/' \
-			-e 's/\("id": ".*\)@/\1_beta@/' \
-			-e 's/\("strict_min_version"\): ".*"/\1: "140.0"/' \
-			manifest.json > manifest.json.beta
-		mv -f manifest.json.beta manifest.json
-	fi
-}
-
 # Backup manifest.json
 mv manifest.json manifest_firefox.json
 
 # Firefox
 cp -p manifest_firefox.json manifest.json
-for_beta
 zip -r ../$XPI_NAME.zip * -x "manifest_*.json"
 mv -f ../$XPI_NAME.zip ../$XPI_NAME.xpi
+
+# Firefox Beta
+sed \
+	-e 's/"version": "\(.*\)"/"version": "\1"\, "version_name": "\1 beta"/' \
+	-e 's/\("id": ".*\)@/\1_beta@/' \
+	-e 's/\("strict_min_version"\): ".*"/\1: "140.0"/' \
+	manifest_firefox.json > manifest.json
+zip -r ../$XPI_NAME-beta.zip * -x "manifest_*.json"
+mv -f ../$XPI_NAME-beta.zip ../$XPI_NAME-beta.xpi
 
 # Chrome
 cp -p manifest_chrome.json manifest.json
